d604dacfe530fe12fb894242997fbf4a772cfc74
Jaychandapara7 <jaychandapara@gmail.com>
2026-01-06 12:02:57 +0530

    Add files via upload

---
+++ div.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,div;
+    printf("The first number is :");
+    scanf("%d",&a);
+
+    printf("The second number is :");
+    scanf("%d",&b);
+
+    div=a/b;
+    printf("The divi s : %d/n",div);
+    return 0;
+}
